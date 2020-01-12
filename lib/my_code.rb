3fa452dbb98d4c568e45cb4067d3a38d0c008751
Franco Canedo <francanedo1@hotmail.com>
2020-01-12 19:05:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,14 +50,20 @@ end
 def reduce_to_total(source_array, starting_point)
   i = 0 
   total = 0
-  while i < source_array.length do
+  totalS = starting_point
+  
     if starting_point == true
-      
+      while i < source_array.length do
+        element = source_array[i]
+        i += 1 
+        totalS += element
+      end
+      return totalS
     end 
     element = source_array[i]
     i += 1 
     starting_point += element 
-  end 
+  
   total += starting_point
   total
 end 
