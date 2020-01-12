cc8978acbbc30235b4f608b86f6786fbe70d9634
Franco Canedo <francanedo1@hotmail.com>
2020-01-12 18:46:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,7 +53,7 @@ def reduce_to_total(source_array, starting_point)
   while i < source_array.length do
     element = source_array[i]
     i += 1 
-    total += element
+    total += element + starting_point
   end 
   total
 end 
