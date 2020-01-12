e91a6962cad6a3432ee2dd76aeb07e78a2fc507b
Franco Canedo <francanedo1@hotmail.com>
2020-01-12 18:34:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,12 @@
 # My Code here....
 def map_to_negativize(source_array) 
   i = 0 
+  new_array = []
   while i < source_array.length do
-    source_array[i] * -1
+    element = source_array[i] * -1
+    new_array << element
     i += 1 
+    
   end 
   source_array
 end 
