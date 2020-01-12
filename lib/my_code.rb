baffc66942728783d57752b1438805d37dfb3cb6
Franco Canedo <francanedo1@hotmail.com>
2020-01-12 18:38:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -36,7 +36,15 @@ def map_to_double(source_array)
 end 
 
 def map_to_square(source_array)
-  
+  i = 0 
+  new_array = []
+  while i < source_array.length do
+    element = source_array[i] ** 2
+    new_array << element
+    i += 1 
+    
+  end 
+  new_array
 end 
 
 
