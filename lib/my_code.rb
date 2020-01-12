e5ad185807a111f088a0e7ca31a23283a7e12cbb
Franco Canedo <francanedo1@hotmail.com>
2020-01-12 18:37:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -24,5 +24,13 @@ def map_to_no_change(source_array)
 end 
 
 def map_to_double(source_array)
-  
+  i = 0 
+  new_array = []
+  while i < source_array.length do
+    element = source_array[i] * 2
+    new_array << element
+    i += 1 
+    
+  end 
+  new_array
 end 
